63f98c0caf069bdd91a06c7a8b1e0d8042700a1d
Fine <skim.gm@gmail.com>
2025-07-10 16:18:27 +0900

    Revert "#2 [feat] 3D 모델링 드래그&드랍, 크기조절, 회전 구현"
    This reverts commit 7c338c9f1f232152e3e6f2d28ed5553a075f3e28.

--- WhimsyLand/ContentView.swift
+++ WhimsyLand/ContentView.swift
@@ -11,64 +11,37 @@ import RealityKitContent
 
 struct ContentView: View {
 
-// enlarge 만이 아니라 drag & drop, magnify, rotate 제스처를 위해 enlarge 부분 삭제 - Fine
-//    @State private var enlarge = false
+    @State private var enlarge = false
 
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
             if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
-                // scene에 대해 제스처 적용이 가능하도록 함 - Fine
-                enableGesturesRecursively(for: scene)
                 content.add(scene)
             }
-// enlarge 부분 삭제 - Fine
-//        } update: { content in
-//            // Update the RealityKit content when SwiftUI state changes
-//            if let scene = content.entities.first {
-//                let uniformScale: Float = enlarge ? 1.4 : 1.0
-//                scene.transform.scale = [uniformScale, uniformScale, uniformScale]
-//            }
+        } update: { content in
+            // Update the RealityKit content when SwiftUI state changes
+            if let scene = content.entities.first {
+                let uniformScale: Float = enlarge ? 1.4 : 1.0
+                scene.transform.scale = [uniformScale, uniformScale, uniformScale]
+            }
         }
-//        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-//            enlarge.toggle()
-//        })
-
-// 모든 엔티티에 대해 Tap, Drag, magnify, rotate 제스처 추가
-        .gesture(
-            TapGesture()
-                .targetedToAnyEntity()
-                .useGestureComponent()
-        )
-        .gesture(
-            DragGesture()
-                .targetedToAnyEntity()
-                .useGestureComponent()
-        )
-        .gesture(
-            MagnifyGesture()
-                .targetedToAnyEntity()
-                .useGestureComponent()
-        )
-        .gesture(
-            RotateGesture3D()
-                .targetedToAnyEntity()
-                .useGestureComponent()
-        )
+        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
+            enlarge.toggle()
+        })
         .toolbar {
             ToolbarItemGroup(placement: .bottomOrnament) {
-// enlarge 부분 삭제 - Fine
-//                VStack (spacing: 12) {
-//                    Button {
-//                        enlarge.toggle()
-//                    } label: {
-//                        Text(enlarge ? "Reduce RealityView Content" : "Enlarge RealityView Content")
-//                    }
-//                    .animation(.none, value: 0)
-//                    .fontWeight(.semibold)
-//
+                VStack (spacing: 12) {
+                    Button {
+                        enlarge.toggle()
+                    } label: {
+                        Text(enlarge ? "Reduce RealityView Content" : "Enlarge RealityView Content")
+                    }
+                    .animation(.none, value: 0)
+                    .fontWeight(.semibold)
+
                     ToggleImmersiveSpaceButton()
-//                }
+                }
             }
         }
     }
--- WhimsyLand/Model/Module.swift
+++ WhimsyLand/Model/Module.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 /// A description of the modules that the app can present.
-///  error 주석 처리 - Fine
-//enum Module: String, Identifiable, CaseIterable, Equatable {
-//}
+enum Module: String, Identifiable, CaseIterable, Equatable {
+}
 
