26ad07c6a38a038f39f24aa3ff37566ac8823b0b
HwangJeha <155410554+Jeha0101@users.noreply.github.com>
2025-07-14 11:22:22 +0900

    test: 테스트 코드

---
+++ WhimsyLand/Views/ObjectPlacementMenuView.swift
@@ -0,0 +1,46 @@
+// git 올리기 전에 삭제하기
+
+import SwiftUI
+
+struct ObjectPlacementMenuView: View {
+    let appState: AppState
+
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
+    
+    @State private var presentConfirmationDialog = false
+
+    var body: some View {
+        VStack(spacing: 20) {
+            ObjectSelectionView(
+                modelDescriptors: appState.modelDescriptors,
+                selectedFileName: appState.selectedFileName
+            ) { descriptor in
+                if let model = appState.placeableObjectsByFileName[descriptor.fileName] {
+                    appState.placementManager?.selectObject(model)
+                }
+            }
+
+            Button("Remove all objects", systemImage: "trash") {
+                presentConfirmationDialog = true
+            }
+            .font(.subheadline)
+            .buttonStyle(.borderless)
+            .confirmationDialog("Remove all objects?", isPresented: $presentConfirmationDialog) {
+                Button("Remove all", role: .destructive) {
+                    Task {
+                        await appState.placementManager?.removeAllPlacedObjects()
+                    }
+                }
+            }
+
+            Button("Leave", systemImage: "xmark.circle") {
+                Task {
+                    await dismissImmersiveSpace()
+                    appState.didLeaveImmersiveSpace()
+                }
+            }
+            .font(.subheadline)
+            .buttonStyle(.borderless)
+        }
+    }
+}
