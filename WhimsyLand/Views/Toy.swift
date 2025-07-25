90749f0dd366bbc80c66b66ac00af9e9c830a106
changhyen yun <ychcom@naver.com>
2025-07-25 09:51:32 +0900

    하나의 윈도우에서 콘텐츠만 변경하는 방버
    toy데이터를 전역 상태로 관리하도록함

--- WhimsyLand/Views/Toy.swift
+++ WhimsyLand/Views/Toy.swift
@@ -10,7 +10,7 @@ import RealityKit
 import RealityKitContent
 
 // 모델 설정 상수
-private let modelName = "Cube"  // TODO: 선택한 toy3D name 받아오기
+//private let modelName : String = ""  // TODO: 선택한 toy3D name 받아오기
 private let targetSizeInMeters: Float = 0.26
 //private let modelDepth: Double = 200
 //private let modelSize: CGFloat = 560
@@ -18,31 +18,28 @@ private let targetSizeInMeters: Float = 0.26
 //private let modelOrientation: SIMD3<Double> = [0, 0, 0]
 
 struct Toy: View {
-//    @Environment(ViewModel.self) private var model
-    //    @State private var conePosition: SIMD3<Float> = .zero
-    @State private var sceneEntity: Entity? = nil
+ 
+    var modelName: String = ""
+    @State private var loadedEntity: Entity?
     
     var body: some View {
         // 3D 미리보기
         RealityView { content in
-            if let entity = try? await Entity(named: modelName) {
-                // 모델 원본 크기 측정
-                let originalBounds = entity.visualBounds(relativeTo: nil)
-                let originalSize = originalBounds.extents
-                
-                // 모델 크기 중 가장 큰 축을 기준으로 스케일 계산
-                let maxOriginalDimension = max(originalSize.x, originalSize.y, originalSize.z)
-                let scaleFactor = targetSizeInMeters / maxOriginalDimension
-                
-                entity.scale = [scaleFactor, scaleFactor, scaleFactor]
-                entity.position =  [0.21, -0.13, 0]
-                
-                enableGesturesRecursively(for: entity)
+            if let entity = loadedEntity {
                 content.add(entity)
-                sceneEntity = entity
             }
+           
+        } update: { content in
+            content.entities.removeAll()
+            if let entity = loadedEntity {
+                           content.add(entity)
+                       }
+        
         } placeholder: {
             ProgressView()
+        }
+        .task(id: modelName) {
+            await loadModel()
         }
         .gesture(
             RotateGesture3D()
@@ -69,6 +66,28 @@ struct Toy: View {
         //            .dragRotation(yawLimit: .degrees(360), pitchLimit: .degrees(360))
         //            .offset(z: modelDepth)
     }
+    
+    private func loadModel() async {
+              if let entity = try? await Entity(named: modelName) {
+                  
+                  // 모델 원본 크기 측정
+                  let originalBounds = entity.visualBounds(relativeTo: nil)
+                  let originalSize = originalBounds.extents
+                  
+                  // 모델 크기 중 가장 큰 축을 기준으로 스케일 계산
+                  let maxOriginalDimension = max(originalSize.x, originalSize.y, originalSize.z)
+                  let scaleFactor = targetSizeInMeters / maxOriginalDimension
+                  
+                  entity.scale = [scaleFactor, scaleFactor, scaleFactor]
+                  entity.position =  [0.21, -0.13, 0]
+                  
+                  enableGesturesRecursively(for: entity)
+                  
+                  await MainActor.run {
+                      loadedEntity = entity
+                  }
+              }
+      }
 }
 
 //#Preview {
